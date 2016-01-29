baa3359c9cb7ea2030dbf91801958f00962e766b
lumig242 <lumig242@gmail.com>
2016-01-28 22:14:25 -0800

    add the test application

--- src/application/TestClass.java
+++ src/application/TestClass.java
@@ -3,9 +3,53 @@ package application;
 import config.Message;
 import core.MessagePasser;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
+
+
 public class TestClass {
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args){
-		final MessagePasser mp1 = new MessagePasser("Configuration.yaml", "alice");
+		final String localName;
+		// Get the iter Times
+		int l = 1;
+		if(args.length > 0){
+			try{
+				l = Integer.parseInt(args[0]);
+			}catch(Exception e){
+				System.out.println("Invalid number input! Will only send once");
+			}
+		}
+		final int limit = l; 
+		
+		// Parse the application configuration
+		Yaml yaml = new Yaml();
+		InputStream input;
+		try {
+			input = new FileInputStream(new File("ApplicationConfig.yaml"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return;
+		}
+		Map<String, Object> values = (Map<String, Object>) yaml.load(input);
+		localName = (String) values.get("name");
+		final ArrayList<Message> sendMessages = new ArrayList<Message>();
+		ArrayList<Map<String, String>> msgs = (ArrayList<Map<String, String>>) values.get("send");
+		for(Map<String, String> msg: msgs){
+			sendMessages.add(new Message(msg.get("dest"), msg.get("kind"), msg.get("data")));
+		}
+		// Get all the messages to send
+		System.out.println(sendMessages);
+		
+		// Start the application
+		// Receiver thread
+		final MessagePasser mp = new MessagePasser("Configuration.yaml", localName);
 		Thread recieve1 = new Thread() {
             public void run() {
             	while(true){
@@ -15,68 +59,41 @@ public class TestClass {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-            		Message msg = mp1.receive();
+            		Message msg = mp.receive();
             		if(msg != null){
-            			System.out.println("Recieve in the application alice///" + msg);
+            			System.out.println("Recieve in the application " + localName + " : " + msg);
             		}
             	}
             }
         };
         recieve1.start();
         
+        try {
+        	System.out.println("Waiting for all the nodes to be set!");
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        // Sender thread
 		Thread send1 = new Thread() {
             public void run() {
             	while(true){
-	            	try {
-						Thread.sleep(2000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-	                mp1.send(new Message("bob", "test", "testdata!!!!!"));
-            	}
-            }
-        };
-        send1.start();
-        
-        
-        
-        
-        final MessagePasser mp2 = new MessagePasser("Configuration.yaml", "bob");
-		Thread recieve2 = new Thread() {
-            public void run() {
-            	while(true){
-            		try {
-						Thread.sleep(500);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-            		Message msg = mp2.receive();
-            		if(msg != null){
-            			System.out.println("Recieve in the application bob///" + msg);
+            		for(int i = 0; i < limit; i++){
+            			for(Message msg: sendMessages){
+			            	try {
+								Thread.sleep(2000);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+			                mp.send(msg);
+            			}
             		}
             	}
             }
         };
-        recieve2.start();
-        
-
-		Thread send2 = new Thread() {
-            public void run() {
-            	while(true){
-	            	try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-	                mp2.send(new Message("alice", "test", "testdata!!!!!"));
-            	}
-            }
-        };
-        send2.start();
-        
-        
+        send1.start();
 	}
 }
